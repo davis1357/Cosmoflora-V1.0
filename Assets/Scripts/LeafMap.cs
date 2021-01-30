9d1966e13fcaf5a338a73bff3c72814e247227da
FailedSuccessfully <itay.fichman@gmail.com>
2021-01-30 10:33:32 +0200

    i m tree builder

---
+++ Assets/Scripts/LeafMap.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+public class LeafMap : MonoBehaviour
+{
+    private Dictionary<Colors, Sprite> dict;
+    private AsyncOperationHandle opHandle;
+
+    internal async void Init(TreeMaster master){
+        var op = LoadVariants(master.GetLeaf());
+        await op.Task;
+        ApplyColor(master.GetColor());
+    }
+
+
+    private AsyncOperationHandle<IList<GameObject>> LoadVariants(LeafShape leaf){
+        dict = new Dictionary<Colors, Sprite>();
+        AsyncOperationHandle<IList<GameObject>> myOut;
+        myOut = Addressables.LoadAssetsAsync<GameObject>(leaf.ToString(), null);
+        myOut.Completed += (operation) =>
+        {
+            Colors curClr;
+            foreach(GameObject a in operation.Result){
+                if (System.Enum.TryParse<Colors>(a.name.Substring(a.name.IndexOf('_') + 1), out curClr)){
+                    dict.Add( curClr, a.GetComponent<SpriteRenderer>().sprite);
+                    //Debug.Log(curClr.ToString());
+                }
+            }
+            if (opHandle.IsValid()){
+                this.enabled = false;
+                UnloadVariant(opHandle);
+            }
+            opHandle = operation; 
+        };
+        return myOut;
+    }
+
+    private void UnloadVariant(AsyncOperationHandle lastOp){
+        Addressables.Release(lastOp);
+    }
+    private void OnDestroy() {
+        UnloadVariant(opHandle);
+    }
+
+    internal void ApplyColor(Colors clr){
+        List<Leaf> leaves = new List<Leaf>();
+        GetComponentsInChildren<Leaf>(leaves);
+        SpriteRenderer r;
+        foreach (Leaf l in leaves){
+            //Debug.Log($"leaf is {l.name} in {l.transform.localPosition.x}, {l.transform.localPosition.y}");
+            r = l.GetComponent<SpriteRenderer>();
+            r.sprite = dict[clr];
+            r.enabled = true;
+        }
+        this.enabled = true;
+        //Debug.Log(dict.Keys.Count);
+    }
+}
--- Assets/Scripts/TreeMaster.cs
+++ Assets/Scripts/TreeMaster.cs
@@ -1,48 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 #region Enums
-    public enum TreeShape{zero, fortyfive, ninety};
+public enum TreeShape{zero, fortyfive, ninety};
     public enum Colors{B, G, O, P, R, Y};
-    public enum LeafShape{Fall, Round, Drapes};
-#endregion
-
-#region Consts
-public static class MyConstants {
-    public const string pathPrefabs="";
-    public const string pathTrunks="";
-    public const string pathLeaves="";
-}
+    public enum LeafShape{fall, round, stringy};
 #endregion
 
 public class TreeMaster : MonoBehaviour
 {
     // Publics
-    public GameObject treeFab;
-    public GameObject leaf, trunk;
 
-    protected Dictionary<TreeShape, GameObject> dict;
     // Privates
+    [SerializeField]
+    private List<AssetReference> _prefabs;
+    private TreeShape _myShape;
+    private Colors _myColor;
+    private LeafShape _myLeaf;
+    private TreeBase _myBase;
 
 
     private void Awake() {
-        dict = new Dictionary<TreeShape, GameObject>();
+        foreach (AssetReference _prefab in _prefabs){
+            _prefab.LoadAssetAsync<GameObject>().Completed += (operation) =>
+                {
+                    //Debug.Log($"{operation.Result.name} is done");
+                };
+        }
+        SetShape(TreeShape.zero);
+        SetColor(Colors.B);
+        SetLeaf(LeafShape.fall);
     }
     // Start is called before the first frame update
     void Start()
     {
         SetBase();
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    internal async void SetBase(){
+        if (!(_myBase == null)){
+            Destroy(_myBase.gameObject);
+        }
+        Debug.Log($"ifman {!(_myBase == null)}");
+        int index = (int)_myShape;
+        //Debug.Log(_prefabs.Count);
+        if (_prefabs.Count <= index)
+            return;
+        AssetReference baseAsset = _prefabs[index];
+        await baseAsset.OperationHandle.Task;
+        if (!baseAsset.RuntimeKeyIsValid())
+            return;
+        //Instantiate(this.treeFab, Vector3.zero, Quaternion.identity).AddComponent<TreeBase>().Init(this);
+        baseAsset.InstantiateAsync(Vector3.zero, Quaternion.identity, this.transform).Completed += (operation) => {
+            _myBase = operation.Result.GetComponent<TreeBase>();
+            _myBase.Init(this);
+        };
     }
 
-    private void SetBase(){
-        Instantiate(this.treeFab, Vector3.zero, Quaternion.identity).AddComponent<TreeBase>().Init(this);
+    public void SetShape(TreeShape shape){
+        _myShape = shape;
     }
+    public TreeShape GetShape(){
+        return _myShape;
+    }
+    public void SetColor(Colors color){
+        _myColor = color;
+    }
+    public Colors GetColor(){
+        return _myColor;
+    }
+    public void SetLeaf(LeafShape leaf){
+        _myLeaf = leaf;
+    }
+    public LeafShape GetLeaf(){
+        return _myLeaf;
+    }
+
 }
